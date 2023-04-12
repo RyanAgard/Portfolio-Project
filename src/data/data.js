4a3fdd95199b34b7447f1f72f8c637c71941d315
Ryan Agard <ryantagard@yahoo.com>
2023-04-12 15:12:40 -0400

    small changes to the work links

--- src/data/data.js
+++ src/data/data.js
@@ -6,30 +6,30 @@ export const data=[
         id:1,
         name:"Full Stack Application",
         image:Fullstackwebsite ,
-        github:"https://github.com/RyanAgard",
+        github:"https://github.com/RyanAgard/Full-Stack-MERN-App",
         live:"",
     },
     {
         id:2,
         name:"React JS Application",
         image:Reactapplication,
-        github:"https://github.com/RyanAgard",
+        github:"https://github.com/RyanAgard/Second-React-Application",
         live:"",
     },
-    {
-        id:3,
-        name:"React JS Application",
-        image:"",
-        github:"",
-        live:"",
-    },
-    {
-        id:4,
-        name:"React JS Application",
-        image:"",
-        github:"",
-        live:"",
-    },
+    // {
+    //     id:3,
+    //     name:"React JS Application",
+    //     image:"",
+    //     github:"",
+    //     live:"",
+    // },
+    // {
+    //     id:4,
+    //     name:"React JS Application",
+    //     image:"",
+    //     github:"",
+    //     live:"",
+    // },
 
 
 ]
