a4ef85832b95bf852ec990391278cbd52199e312
Ryan Agard <ryantagard@yahoo.com>
2023-04-13 13:41:57 -0400

    email button now working

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -117,7 +117,7 @@ const Navbar = () => {
           <li className='w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-[#6fc2b0]'>
             <a
               className='flex justify-between items-center w-full text-gray-300'
-              href='/'
+              href='mailto:ryantagard@yahoo.com?subject=&cc=&bcc&body='
             >
               Email <HiOutlineMail size={30} />
             </a>
