a68a1965b1479da0bfd0ccb74d589277dc1eb63d
Ryan Agard <ryantagard@yahoo.com>
2023-04-13 07:44:22 -0400

    added 3rd project to works

--- src/data/data.js
+++ src/data/data.js
@@ -1,6 +1,6 @@
 import Fullstackwebsite from '../assets/projects/Fullstackwebsite.png'
 import Reactapplication from '../assets/projects/Reactapplication.png'
-
+import Pingponggame from '../assets/projects/Pingponggame_edited.jpg'
 export const data=[
     {
         id:1,
@@ -16,13 +16,13 @@ export const data=[
         github:"https://github.com/RyanAgard/Second-React-Application",
         live:"",
     },
-    // {
-    //     id:3,
-    //     name:"React JS Application",
-    //     image:"",
-    //     github:"",
-    //     live:"",
-    // },
+    {
+        id:3,
+        name:"JavaScript,HTML & CSS Game",
+        image:Pingponggame,
+        github:"https://github.com/RyanAgard/Mod-1---Game-On-",
+        live:"",
+    },
     // {
     //     id:4,
     //     name:"React JS Application",
