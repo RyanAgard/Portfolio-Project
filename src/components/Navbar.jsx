49fc2b75b0db6bc8d53764fa8a64dc55e4b3a2ff
Ryan Agard <ryantagard@yahoo.com>
2023-04-18 18:02:19 -0400

    linkedin working

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -101,7 +101,7 @@ const Navbar = () => {
           <li className='w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-blue-600'>
             <a
               className='flex justify-between items-center w-full text-gray-300'
-              href='https://www.linkedin.com/public-profile/settings?trk=d_flagship3_profile_self_view_public_profile'
+              href='https://www.linkedin.com/in/ryan-agard/'
             >
               Linkedin <FaLinkedin size={30} />
             </a>
