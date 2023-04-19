4182c65242aa74e09ccd9636aa8859663d4e285c
Ryan Agard <ryantagard@yahoo.com>
2023-04-19 10:27:10 -0400

    small changes to phone icons

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -62,7 +62,7 @@ const Navbar = () => {
         className={
           !nav
             ? 'hidden'
-            : 'absolute top-0 left-0 w-full h-screen bg-[#ffffff] flex flex-col justify-center items-center'
+            : 'absolute top-0 left-0 w-full h-screen bg-[#fa961c] flex flex-col justify-center items-center'
         }
       >
         <li className='py-6 text-4xl'>
